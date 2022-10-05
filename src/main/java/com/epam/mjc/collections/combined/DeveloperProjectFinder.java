1ab2ad65d09b805f77e1da44ed2b5f42cbdbff2b
ShulpinP <96373490+ShulpinP@users.noreply.github.com>
2022-10-05 14:35:40 +0300

    1

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,33 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        Comparator<String> stringComparator = new StringLengthSort();
+        for (Map.Entry<String, Set<String>> e:projects.entrySet()) {
+            for (String str: e.getValue()) {
+                if (str.equals(developer)) {
+                    result.add(e.getKey());
+                }
+            }
+        }
+        Collections.sort(result,stringComparator);
+        return result;
+    }
+}
+class StringLengthSort implements Comparator<String>{
+    @Override
+    public int compare(String str1, String str2) {
+        if(str1.length() < str2.length()){
+            return 1;
+        }else{
+            if(str1.length() > str2.length()){
+                return -1;
+            }else{
+                return 0;
+            }
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,32 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Set<Integer> lengthList = new HashSet<>();
+        Set<String> words = new HashSet<>();
+        Map <Integer, Set<String>> result = new HashMap<>();
+        for (Map.Entry<String,Integer> e:sourceMap.entrySet()) {
+            lengthList.add(e.getKey().length());
+        }
+        for (int i:lengthList) {
+            words.clear();
+            for (Map.Entry<String,Integer> e:sourceMap.entrySet()) {
+                if (e.getKey().length() == i) {
+                    words.add(e.getKey());
+                }
+            }
+            Set<String> over = new HashSet<>();
+            over.addAll(words);
+            result.put(i,over);
+
+
+
+        }
+        return result;
     }
 }
